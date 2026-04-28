eee9dd2e4b39b0e4634ea7dd7178f2cd3f2df9a2
Corey Long <hello@coreylong.dev>
2026-04-28 12:36:40 -0400

    variables created for both classes

--- PortfolioManager.java
+++ PortfolioManager.java
@@ -1,9 +1,20 @@
+import java.util.*;
+
+/**
+ * PortfolioManager.java
+ * Main class for the program. manages transactions in the stock portfolio. 
+ * Author: Corey Long
+ * Date: Apr 27, 2028
+ */
+
 // IMPORTS
 
 
 // START MAIN CLASS
 public class PortfolioManager {
-    public static void main(String[] args) {
 
+    private ArrayList<TransactionHistory> portfolioList = new ArrayList<>();
+    public static void main(String[] args) {
+        
     }
 }
--- TransactionHistory.java
+++ TransactionHistory.java
@@ -1,3 +1,13 @@
+/**
+ * TransactionHistory.java
+ * Store the records for PortfolioManager class
+ * Author: Corey Long
+ * Date: Apr 27, 2028
+ */
 public class TransactionHistory {
-
+    private String ticker;
+    private String transDate;
+    private String transType;
+    private double qty;
+    private double costBasis;
 }
